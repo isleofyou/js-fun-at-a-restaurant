f20670b7a9a53584d039034b7e4cb317eb63cfbb
David Tran <frozenpho@gmail.com>
2021-08-26 21:35:43 -0600

    Add meal functions

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,45 @@
-function nameMenuItem() {
-
+function nameMenuItem(newFood) {
+  return menuItemName =`Delicious ${newFood}`;
 }
 
+function createMenuItem(menuItemName,price,type) {
+var menuItem = {
+    name: menuItemName,
+    price: price,
+    type: type
+};
+  return menuItem;
+}
 
+function addIngredients(ingredient, ingredients) {
+  if (ingredients.includes(ingredient)) {
+    return ingredients;
+  } else {
+  return ingredients.push(ingredient);
+  }
+};
+
+function formatPrice(initialPrice){
+  formattedPrice = `$${initialPrice}`;
+  return formattedPrice;
+}
+
+function decreasePrice (initialPrice){
+  decreasedPrice = initialPrice - initialPrice/10;
+  return decreasedPrice;
+}
+
+function createRecipe (title,ingredients,type){
+  recipe = {title: title,
+            ingredients: ingredients,
+            type: type};
+  return recipe;
+}
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
