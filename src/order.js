b3b3702c4e5ba3557869efbefa8caf6c3b0de1a5
David Tran <frozenpho@gmail.com>
2021-08-28 12:10:21 -0600

    Write order functions

--- src/order.js
+++ src/order.js
@@ -1,7 +1,36 @@
+function takeOrder (newOrder, deliveryOrders){
+  if (deliveryOrders.length < 3){
+  deliveryOrders.push(newOrder);
+  return deliveryOrders;
+} else {return deliveryOrders}
+}
 
+function refundOrder (refundNumber, deliveryOrders){
+  for (i = 0; i < deliveryOrders.length; i++){
+  if (deliveryOrders[i].orderNumber === refundNumber) {
+    deliveryOrders.splice([i],1);
+    return deliveryOrders;
+    }
+  }
+}
+
+function listItems (deliveryOrders) {
+  items = [];
+  for (var i = 0; i < deliveryOrders.length; i++){
+    items.push(deliveryOrders[i].item);
+  } return items.join(', ');
+}
+
+function searchOrder (deliveryOrders, checkItem){
+  for (i = 0; i < deliveryOrders.length; i++){
+    if (deliveryOrders[i].item.includes(checkItem)){
+  return true;
+    }
+  } return false;
+}
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
